1e180cb68f66c784f6bc70a5d7584c282e0ec573
Guillaume Boutry <guillaume.boutry@canonical.com>
2023-10-05 10:39:39 +0200

    Add secrets service
    Add barbican, mysql applications, integrate to vault if both are
    enabled.

--- main.tf
+++ main.tf
@@ -627,3 +627,45 @@ resource "juju_application" "vault" {
 
   units = 1
 }
+
+module "mysql-barbican" {
+  count      = var.enable-barbican ? (var.many-mysql ? 1 : 0) : 0
+  source     = "./modules/mysql"
+  model      = juju_model.sunbeam.name
+  name       = "mysql"
+  channel    = var.mysql-channel
+  scale      = var.ha-scale
+  many-mysql = var.many-mysql
+  services   = ["barbican"]
+}
+
+module "barbican" {
+  count                = var.enable-barbican ? 1 : 0
+  source               = "./modules/openstack-api"
+  charm                = "barbican-k8s"
+  name                 = "barbican"
+  model                = juju_model.sunbeam.name
+  channel              = var.barbican-channel
+  rabbitmq             = module.rabbitmq.name
+  mysql                = var.many-mysql ? module.mysql-barbican[0].name["barbican"] : "mysql"
+  keystone             = module.keystone.name
+  ingress-internal     = juju_application.traefik.name
+  ingress-public       = juju_application.traefik-public.name
+  scale                = var.os-api-scale
+  mysql-router-channel = var.mysql-router-channel
+}
+
+resource "juju_integration" "barbican-to-vault" {
+  count = (var.enable-barbican && var.enable-vault) ? 1 : 0
+  model = juju_model.sunbeam.name
+
+  application {
+    name     = module.barbican[count.index].name
+    endpoint = "vault-kv"
+  }
+
+  application {
+    name     = juju_application.vault[count.index].name
+    endpoint = "vault-kv"
+  }
+}
