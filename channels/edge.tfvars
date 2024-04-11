ae686cd9f31c8913172771b94db64399566d9281
James Page <james.page@canonical.com>
2024-04-11 10:57:16 +0100

    Use stable channels by default
    Update to use stable by default, test with edge in workflows.

---
+++ channels/edge.tfvars
@@ -0,0 +1,8 @@
+# Core channels for direct charm depdndencies
+openstack-channel = "2023.2/edge"
+bind-channel      = "9/edge"
+# Test with candidate for dependent projects
+mysql-channel        = "8.0/candidate"
+mysql-router-channel = "8.0/candidate"
+traefik-channel      = "1.0/candidate"
+rabbitmq-channel     = "3.12/candidate"
--- variables.tf
+++ variables.tf
@@ -16,13 +16,13 @@
 variable "openstack-channel" {
   description = "Operator channel for OpenStack deployment"
   type        = string
-  default     = "2023.2/edge"
+  default     = "2023.2/stable"
 }
 
 variable "mysql-channel" {
   description = "Operator channel for MySQL deployment"
   type        = string
-  default     = "8.0/candidate"
+  default     = "8.0/stable"
 }
 
 variable "mysql-revision" {
@@ -41,14 +41,14 @@ variable "mysql-config" {
 
 variable "mysql-router-channel" {
   description = "Operator channel for MySQL router deployment"
-  default     = "8.0/candidate"
+  default     = "8.0/stable"
   type        = string
 }
 
 variable "traefik-channel" {
   description = "Operator channel for Traefik deployment"
   type        = string
-  default     = "1.0/candidate"
+  default     = "1.0/stable"
 }
 
 variable "traefik-revision" {
@@ -66,7 +66,7 @@ variable "traefik-config" {
 variable "rabbitmq-channel" {
   description = "Operator channel for RabbitMQ deployment"
   type        = string
-  default     = "3.12/edge"
+  default     = "3.12/stable"
 }
 
 variable "rabbitmq-revision" {
@@ -102,7 +102,7 @@ variable "certificate-authority-config" {
 variable "ovn-channel" {
   description = "Operator channel for OVN deployment"
   type        = string
-  default     = "23.09/edge"
+  default     = "23.09/stable"
 }
 
 variable "ovn-central-channel" {
@@ -506,7 +506,7 @@ variable "enable-designate" {
 variable "bind-channel" {
   description = "Operator channel for Bind deployment"
   type        = string
-  default     = "9/edge"
+  default     = "9/stable"
 }
 
 variable "bind-revision" {
@@ -558,7 +558,7 @@ variable "enable-vault" {
 variable "vault-channel" {
   description = "Operator channel for Vault deployment"
   type        = string
-  default     = "latest/edge"
+  default     = "1.15/edge"
 }
 
 variable "vault-revision" {
@@ -624,7 +624,7 @@ variable "magnum-config" {
 variable "ldap-channel" {
   description = "Operator channel for Keystone LDAP deployment"
   type        = string
-  default     = "2023.2/edge"
+  default     = "2023.2/stable"
 }
 
 variable "ldap-revision" {
